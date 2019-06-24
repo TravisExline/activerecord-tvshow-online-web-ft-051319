e298690520e62d91b616970e18d48b08a0d9b9d3
Travis Exline <t.exline22@gmail.com>
2019-06-24 16:10:15 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,7 +4,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    Show.where("rating = ?", Show.highest_rating.first).first
+    Show.where("rating = ?", Show.highest_rating).first
   end
 
   def self.lowest_rating
@@ -12,7 +12,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.least_popular_show
-    Show.where("rating = ?", Show.lowest_rating.first).first
+    Show.where("rating = ?", Show.lowest_rating).first
   end
 
   def self.ratings_sum
